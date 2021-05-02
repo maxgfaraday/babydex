9626b16c13e3abb3262a5bc230dcbb0e490f992f
Max G. Faraday <maxgfaraday@6thcolumn.org>
2021-05-02 04:00:45 +0200

    Created wallet and token code and associated migration files

---
+++ contracts/wallet.sol
@@ -0,0 +1,63 @@
+/*
+  SPDX-License-Identifier: Apache-2.0
+
+  Copyright 2021 The 6th Column Project
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity 0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract Wallet is Ownable {
+
+    using SafeMath for uint256;
+
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    // ticker -> Token
+    mapping (bytes32 => Token) public tokenMapping;
+    bytes32[] public tokenList;
+
+    //account_holder -> ticker -> amount
+    mapping (address => mapping(bytes32 => uint256)) public balances;
+
+    modifier tokenExists(bytes32 ticker) {
+        require(tokenMapping[ticker].tokenAddress != address(0), "Unsupported Token");
+        _;
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress) onlyOwner external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) tokenExists(ticker) external {
+        require(amount > 0);
+        require(tokenMapping[ticker].tokenAddress != address(0), "Unsupported Token");
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
+    }
+
+    function withdraw(uint amount, bytes32 ticker) tokenExists(ticker) external {
+        require(amount > 0);
+        require(balances[msg.sender][ticker] >= amount, "Insufficient balance");
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+}
