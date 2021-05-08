b1b7ff4fd80a0d7982a2021c19be3926057daca5
Max G. Faraday <maxgfaraday@6thcolumn.org>
2021-05-08 22:46:16 +0200

    => Creating wallet tests [completed]

--- test/wallet_test.js
+++ test/wallet_test.js
@@ -13,12 +13,23 @@ contract("Dex", accounts => {
             dex.addToken(web3.utils.fromUtf8("LINK"),link.address, {from: accounts[1]})
         )
     })
-    //it("should handle deposits correctly", async () => {
-    //    let dex = await Dex.deployed();
-    //    let link = await Link.deployed();
-    //    await link.approve(dex.address, 500);
-    //    dex.addToken(web3.utils.fromUtf8("LINK"),link.address)
-    //    await dex.deposit(100, web3.utils.fromUtf8("LINK"));
-    //    assert.equal(dex.balances(accounts[0], web3.utils.fromUtf8("LINK")),100);
-    //})
+    it("should handle deposits correctly", async () => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await link.approve(dex.address, 500);
+        dex.addToken(web3.utils.fromUtf8("LINK"),link.address);
+        await dex.deposit(100, web3.utils.fromUtf8("LINK"));
+        let balanceOfLink = await dex.balances(accounts[0],web3.utils.fromUtf8("LINK"));
+        assert.equal(balanceOfLink.toNumber(),100);
+    })
+    it("should handle correct withdrawals correctly", async () => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await TruffleAssert.passes(dex.withdraw(100, web3.utils.fromUtf8("LINK")));
+    })
+    it("should handle faulty withdrawals correctly", async () => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await TruffleAssert.reverts(dex.withdraw(101, web3.utils.fromUtf8("LINK")));
+    })
 })
