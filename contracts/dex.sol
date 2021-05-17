36d7046bfbff09768c8fca77accf12640666c371
Max G. Faraday <maxgfaraday@6thcolumn.org>
2021-05-17 11:33:27 +0200

    [WIP] implemented Bubble Sort for orders, compiles but tests seem to not work

--- contracts/dex.sol
+++ contracts/dex.sol
@@ -22,21 +22,78 @@ import "./Wallet.sol";
 
 contract Dex is Wallet {
 
+    using SafeMath for uint256;
+
     enum Side { BUY, SELL }
 
-        struct Order {
-        uint id;
-        address trader;
-        Side side;
-        bytes32 ticker;
-        uint amount;
-        uint price;
+    struct Order {
+      uint id;
+      address trader;
+      Side side;
+      bytes32 ticker;
+      uint amount;
+      uint price;
     }
 
-    //ticker to type to order
+    uint public nextOrderId = 0;
+
+    //TICKER to TYPE to ORDERS
+    //"ETH"     BUY
     mapping(bytes32 => mapping(uint => Order[])) public orderBook;
 
     function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
         return orderBook[ticker][uint(side)];
     }
+
+    function createLimitOrder(Side side, bytes32 ticker, uint amount, uint price ) public {
+        if(side == Side.BUY) {
+            require(balances[msg.sender][bytes32("ETH")] >= amount.mul(price), "Insufficent ETH");
+
+        }
+        else if(side == Side.SELL) {
+            require(balances[msg.sender][ticker] >= amount);
+
+        }
+
+        Order[] storage orders = orderBook[ticker][uint(side)];
+        orders.push(Order(nextOrderId, msg.sender, side, ticker, amount, price));
+
+        if(orders.length > 1) {
+            //bubble sort - will will traverse from the tail of the array to the 0th index
+            //set the markers for the last index and the penultimate index and the tmp 'buffer' for the swap operation.
+            uint rearIdx = orders.length-1;
+            uint frontIdx = rearIdx-1;
+            Order memory tmp;
+
+            if(side == Side.BUY) { //largest "most interesting" Buy order is closest to 0th position, "the front" (descending order)
+                for(uint i = uint(orders.length); i == 1 ; i--) {
+                    //condition for swapping
+                    if(orders[rearIdx].price >orders[frontIdx].price) {
+                        tmp = orders[frontIdx];
+                        orders[frontIdx] = orders[rearIdx];
+                        orders[rearIdx] =  tmp;
+                    }
+                    frontIdx--;
+                    rearIdx--;
+                }
+            }else if(side == Side.SELL) { //Smallest "most interesting" sell order is at the 0th position, "the front" (ascending order)
+                for(uint i = uint(orders.length); i == 1 ; i--) {
+                    //condition for swapping
+                    if(orders[rearIdx].price < orders[frontIdx].price) {
+                        tmp = orders[frontIdx];
+                        orders[frontIdx] = orders[rearIdx];
+                        orders[rearIdx] =  tmp;
+                    }
+                    frontIdx--;
+                    rearIdx--;
+                }
+            }
+        }
+
+        nextOrderId++;
+    }
+    //
+    //function createMarketOrder() {
+    //
+    //}
 }
---
+++ test/dextest.js
@@ -0,0 +1,73 @@
+const Dex = artifacts.require("Dex");
+const Link = artifacts.require("Link");
+const TruffleAssert = require("truffle-assertions");
+
+contract("Dex:Orderbook", accounts => {
+    //The user must have ETH deposited such that the deposited eth >= buy order value.
+    //Must have enough ETH to cover the order to buy.
+    it("buyer must have more or equivalent amount of currency to create a buy order", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed();
+        await TruffleAssert.reverts(
+            dex.createLimitOrder(Dex.Side.BUY,web3.utils.fromUtf8("LINK"),10,1)
+        )
+        dex.depositEth({value: 10})
+        await TruffleAssert.passes(
+            dex.createLimitOrder(Dex.Side.BUY,web3.utils.fromUtf8("LINK"),10,1)
+        )
+    })
+    //The user must have enough tokens deposited such that token balance >= sell order amount
+    it("seller must have enough or equivalent currency to fulfill a sell order.", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+
+        await TruffleAssert.reverts(
+            dex.createLimitOrder(Dex.Side.SELL,web3.utils.fromUtf8("LINK"),10,1)
+        )
+        await link.approve(dex.address,300)
+        await dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]})
+        await dex.deposit(10,web3.utils.fromUtf8("LINK"))
+        await TruffleAssert.passes(
+            dex.createLimitOrder(Dex.Side.SELL,web3.utils.fromUtf8("LINK"),10,1)
+        )
+    })
+    //the BUY order book should be ordered on price from highest to lowest starting at index[0]
+    it("buy orderbook is ordered by price in descending (hightest [0] to lowest [length-1])", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await link.approve(dex.address,500)
+        await dex.depositEth({value: 3000})
+        await dex.createLimitOrder(Dex.Side.BUY,web3.utils.fromUtf8("LINK"),1,300)
+        await dex.createLimitOrder(Dex.Side.BUY,web3.utils.fromUtf8("LINK"),1,100)
+        await dex.createLimitOrder(Dex.Side.BUY,web3.utils.fromUtf8("LINK"),1,200)
+
+        //[300, 200, 100]
+
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"),Dex.Side.BUY);
+        //console.log(orderbook);
+        assert(orderbook.length > 0)
+        for(let i=0; i < orderbook.length-1; i++) {
+            assert(orderbook[i].price >= orderbook[i+1].price, "buy order book is out of order")
+            //highest price at the beginning
+        }
+    })
+    //the SELL order book should be ordered on price from lowest to highest starting at index[0]
+    it("sell orderbook is ordered by price in ascending (lowest [0] to highest [length-1])", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await link.approve(dex.address,500)
+        await dex.createLimitOrder(Dex.Side.SELL,web3.utils.fromUtf8("LINK"),1,300)
+        await dex.createLimitOrder(Dex.Side.SELL,web3.utils.fromUtf8("LINK"),1,100)
+        await dex.createLimitOrder(Dex.Side.SELL,web3.utils.fromUtf8("LINK"),1,200)
+
+        //[100, 200, 300]
+
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"),Dex.Side.SELL);
+        assert(orderbook.length > 0)
+        for(let i=0; i < orderbook.length-1; i++) {;
+            assert(orderbook[i].price <= orderbook[i+1].price, "sell order book is out of order")
+            //hightest price on the end
+        }
+    })
+
+})
